39af43fcc51f9792cb492f7260ecde0db9af1480
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-10-01 15:40:42 +0530

    Add files via upload

---
+++ student details.py
@@ -0,0 +1,11 @@
+#Author:Daiji kuriakose
+#date:1-10-2024
+#to view student name,ro;; no and cgpa
+
+name=input("enter the name of the student:")
+roll_no=int(input("enter the roll number of the student:"))
+cgpa=float(input("enter the cgpa:"))
+percentage=cgpa*10
+print("name of the student is:",name)
+print("roll number of the student is:",roll_no)
+print("percentage:",percentage)
