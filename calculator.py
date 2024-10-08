4414365261992c0d23246733ae940b8f6626cfb9
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-10-08 15:02:13 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+"""author: Daiji Kuriakose
+   Date  : 08/10/2024
+   Create, concatenate, and print a string and access a sub-string from a given string."""
+num1=int(input ("enter first numbers"))
+num2=int(input("enter second number"))
+num3=int(input("enter third number"))
+summation=(num1+num2)
+print("The sum of number1 and number2 is: ",summation)
+difference=(num2-num1)
+print("The difference when number 2 is subtracted from number1 is: ",difference)
+multiplication=(num1*num2)
+print("The product of number 1 and number2 is: ",multiplication)
+division=(num1/num2)
+print( "The quotient when number1 is divided by number2 is: ",division)
+remainder=(num1%num2)
+print("The remainder when number1 is divided by number2 is: ",remainder)
+result=(num1+num2)*num3/2
+print("The result of (num1+num2)*num3/2 is: ",result)
