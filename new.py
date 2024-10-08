d2c463125e1176790868107312b6a9f26075ae9e
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-10-08 16:26:55 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,28 @@
+"""author: Daiji Kuriakose
+   Date  : 08/10/2024
+    Familiarize time and date in various formats"""
+from datetime import datetime
+current_time=datetime.now()
+print(current_time)
+format1=current_time.now()
+print(format1)
+format2=current_time.strftime("%m/%d/%Y")
+print(format2)
+format3=current_time.strftime("%A,%m %d,%Y")
+print(format3)
+format4=current_time.strftime("%A,%m %d,%Y %H:%M:%S %P")
+print(format4)
+format5=current_time.strftime("%a-%b-%d %H:%M:%S %Z IST %Y")
+print(format5)
+format6=current_time.strftime("%a-%b-%d %H:%m:%s IST %Y")
+print(format6)
+format7=current_time.strftime("%H %M %S")
+print(format7)
+format8=current_time.strftime("%d %m %Y")
+print(format8)
+format9=current_time.strftime("%H %M %S")
+print(format9)
+format10=current_time.strftime("%B")
+print(format10)
+format11=current_time.strftime("%Y")
+print(format11)
