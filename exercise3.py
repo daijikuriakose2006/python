5088969d64b1acdece6a57bab9758942f6a65b71
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-10-15 15:25:07 +0530

    Add files via upload

---
+++ exercise3.py
@@ -0,0 +1,16 @@
+"""
+Author: daiji Kuriakose
+Date:10-10-2024
+Title:Python program to determine the smallest of three number
+"""
+number1=int(input("Enter first number"))
+number2=int(input("Enter second number"))
+number3=int(input("Enter third number"))
+if (number1<number2)and(number1<number3):
+    print("first number",number1,"is smaller")
+elif (number2<number1)and(number2<number3):
+    print("second number",number2,"is smaller")
+elif (number3<number1)and(number3<number2):
+    print("third number",number3,"is smaller")
+else:
+    print("three numbers are equal")
