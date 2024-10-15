9d3bc0ac71f431bfa7907a0e496c5286a3339ddd
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-10-15 14:41:33 +0530

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,10 @@
+"""
+Author: daiji Kuriakose
+Date:10-10-2024
+Title:Python program to find simple interest
+"""
+amount=int(input("Enter the amount"))
+years=float(input("Enter the number of years"))
+rate=float(input("Enter the interest rate"))
+si=(amount*years*rate)/100
+print("simple interest=",si)
