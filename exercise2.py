4aa183d26506412d349ecb4eccb475b653e99a7d
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-10-15 14:59:14 +0530

    Add files via upload

---
+++ exercise2.py
@@ -0,0 +1,13 @@
+"""
+Author: daiji Kuriakose
+Date:10-10-2024
+Title:Python program to determine the largest of two numbers
+"""
+first_number=int(input("Enter the first number"))
+second_number=int(input("Enter the second number"))
+if first_number>second_number:
+    print("First number is larger")
+elif first_number<second_number:
+      print("second number is larger")
+else:
+    print("Two numbers are equal")
