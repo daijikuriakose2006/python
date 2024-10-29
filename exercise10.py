650df74208c1a33b0983e62d64ffd9be39647185
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-10-29 16:22:56 +0530

    Update exercise10.py

--- exercise10.py
+++ exercise10.py
@@ -6,7 +6,11 @@ Title:Python program to find the number of vowels in a string
 string_input=input("Enter a string")
 vowels="aeiouAEIOU"
 vowels_count=0
+consonants_count=0
 for char in string_input:
     if char in vowels:
         vowels_count+=1
+    else:
+        consonants_count+=1
 print(f"No of vowels in the given string {string_input}={vowels_count}")
+print(f"consonants_count in the given string {string_input}={vowels_count}")
