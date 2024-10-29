de1dc0f9840109ce009bc26341966647e686fe16
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-10-29 15:41:04 +0530

    Update exercise8.py

--- exercise8.py
+++ exercise8.py
@@ -1,3 +1,8 @@
+"""
+Author: daiji Kuriakose
+Date:29-10-2024
+Title:Python program to print n prime numbers
+"""
 limit=int(input("enter the number"))
 for number in range (2,limit+1):
     isprime=True
