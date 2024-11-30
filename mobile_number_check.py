87a0ddc9eb38be06c73b68acca36c3c24ab49228
daijikuriakose2006 <daijikuriakose2028@cs.sjcetpalai.ac.in>
2024-11-30 09:42:50 +0530

    Update mobile_number_check.py

--- mobile_number_check.py
+++ mobile_number_check.py
@@ -1,3 +1,6 @@
+"""author: Daiji Kuriakose
+   Date  : 30/11/2024
+   Write a Python program to check weather the mobile number is valid or not:"""
 def mobile_number():
     number=input("Enter number")
     if len(number)==10 and number[0] in '789':
